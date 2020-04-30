49724ae0c51cd27319f14299537d37469f3d7d44
Vladres <vladres121@gmail.com>
2020-04-30 21:43:24 +0300

    Finish Day 30.04

--- NovodBlog/NovodBlog/ClientApp/src/main.ts
+++ NovodBlog/NovodBlog/ClientApp/src/main.ts
@@ -8,5 +8,8 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+setTimeout(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}, 1500);
