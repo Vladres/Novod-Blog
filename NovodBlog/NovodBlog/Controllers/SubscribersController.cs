00780502c266bb71f6ab9443fa2e1c48d0c8b9ec
Vladres <vladres121@gmail.com>
2020-06-01 15:46:54 +0300

    edit the code structure of subscribe controller

--- NovodBlog/NovodBlog/Controllers/SubscribersController.cs
+++ NovodBlog/NovodBlog/Controllers/SubscribersController.cs
@@ -20,13 +20,16 @@ namespace NovodBlog.Controllers
             _context = context;
         }
 
+        #region  GET: api/Subscribers
         // GET: api/Subscribers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Subscribers>>> GetSubscribers()
         {
             return await _context.Subscribers.ToListAsync();
         }
+        #endregion
 
+        #region GET: api/Subscribers/{id}
         // GET: api/Subscribers/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Subscribers>> GetSubscribers(int id)
@@ -40,9 +43,11 @@ namespace NovodBlog.Controllers
 
             return subscribers;
         }
+        #endregion
 
+        #region PUT: api/Subscribers/{id}
         // PUT: api/Subscribers/5
-        
+
         [HttpPut("{id}")]
         public async Task<IActionResult> PutSubscribers(int id, Subscribers subscribers)
         {
@@ -72,8 +77,10 @@ namespace NovodBlog.Controllers
             return NoContent();
         }
 
+        #endregion
+
+        #region POST: api/Subscribers
         // POST: api/Subscribers
-        
         [HttpPost]
         public async Task<ActionResult<Subscribers>> PostSubscribers(Subscribers subscribers)
         {
@@ -82,7 +89,9 @@ namespace NovodBlog.Controllers
 
             return CreatedAtAction("GetSubscribers", new { id = subscribers.id_subscriber }, subscribers);
         }
+        #endregion
 
+        #region DELETE: api/Subscribers/{id}
         // DELETE: api/Subscribers/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Subscribers>> DeleteSubscribers(int id)
@@ -98,10 +107,13 @@ namespace NovodBlog.Controllers
 
             return subscribers;
         }
+        #endregion
 
+        #region Method SubscribersExists 
         private bool SubscribersExists(int id)
         {
             return _context.Subscribers.Any(e => e.id_subscriber == id);
         }
+        #endregion
     }
 }
